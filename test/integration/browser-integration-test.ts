c46e3ebc5f4d89797aeb56747af81b19361afb53
Matt Gaunt <matt@gauntface.co.uk>
2018-03-08 08:45:46 -0800

    what is the server address

--- test/integration/browser-integration-test.ts
+++ test/integration/browser-integration-test.ts
@@ -51,6 +51,7 @@ test.serial('should log in browser', async (t) => {
     });
   });
 
+  console.log(serverAddress);
   const response = await page.goto(`${serverAddress}/build/test/static/logger-integration/`);
   t.deepEqual(response.status(), 200);
 
