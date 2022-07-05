62f0e68b16b5e4ce56ac485311e145870949a045
Matt Gaunt <matt@gaunt.dev>
2022-07-05 15:37:26 -0700

    Fixxing issue with ava using console

--- test/node/node-logger-test.js
+++ test/node/node-logger-test.js
@@ -11,60 +11,45 @@ test.afterEach.always((t) => {
   t.context.sandbox.restore();
 });
 
-test.serial('print with default prefixes', (t) => {
-  const methods = [
-    'debug',
-    'info',
-    'log',
-    'warn',
-    'error',
-    'group',
-    'groupCollapsed',
-  ];
-  const spies = methods.map(m => t.context.sandbox.spy(console, m));
+const methods = [
+  'debug',
+  'info',
+  'log',
+  'warn',
+  'error',
+  'group',
+  'groupCollapsed',
+];
+methods.forEach(m => {
+  // Broken out into individual tests since ava uses console.log()
+  // which can causes the call count to be wrong.
+  test.serial(`logger.${m} with default prefixes`, (t) => {
+    const s = t.context.sandbox.spy(console, m);
+    const MSG = 'hello default prefixes';
+    const logger = new Logger();
 
-  const MSG = 'hello default prefixes';
-
-  const logger = new Logger();
-  for (const m of methods) {
     logger[m](MSG);
     logger.groupEnd();
-  }
 
-  for (const s of spies) {
     t.deepEqual(s.callCount, 1);
     t.deepEqual(s.getCall(0).args[1], MSG);
-  }
-});
-
-test.serial('print with custom prefix string', (t) => {
-  const methods = [
-    'debug',
-    'info',
-    'log',
-    'warn',
-    'error',
-    'group',
-    'groupCollapsed',
-  ];
-  const spies = methods.map(m => t.context.sandbox.spy(console, m));
-
-  const PREFIX = 'logger-test';
-  const MSG = 'hello test prefix';
-
-  const logger = new Logger({
-    prefix: PREFIX,
   });
-  for (const m of methods) {
+
+  test.serial(`logger.${m} with custom prefix string`, (t) => {
+    const s = t.context.sandbox.spy(console, m);
+    const PREFIX = 'logger-test';
+    const MSG = 'hello test prefix';
+    const logger = new Logger({
+      prefix: PREFIX,
+    });
+
     logger[m](MSG);
     logger.groupEnd();
-  }
 
-  for (const s of spies) {
     t.deepEqual(s.callCount, 1);
     t.notDeepEqual(s.getCall(0).args[0].indexOf(PREFIX), -1);
     t.deepEqual(s.getCall(0).args[1], MSG);
-  }
+  });
 });
 
 test.serial('nest groups', (t) => {
